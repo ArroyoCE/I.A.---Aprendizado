10c1ce6c5605244f42795907a2ab7952d85dc1b7
ArroyoCE <rafaeltarroyo@gmail.com>
2024-09-24 13:18:33 -0300

    fixes

--- Image_Transformation_Low_GPU.py
+++ Image_Transformation_Low_GPU.py
@@ -6,16 +6,17 @@ from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
 from PIL import Image
 import os
+from torch.cuda.amp import autocast, GradScaler
 
 
 class TransformationParamNet(nn.Module):
     def __init__(self):
         super(TransformationParamNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
-        self.conv3 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
         self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(256, 9)
+        self.fc = nn.Linear(64, 9)
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
@@ -29,27 +30,37 @@ class TransformationParamNet(nn.Module):
         return x
 
 
-class ImageAlterationDataset(Dataset):
-    def __init__(self, input_dir, target_dir, image_size=(5100, 8400)):
+class PatchedImageDataset(Dataset):
+    def __init__(self, input_dir, target_dir, image_size=(5100, 8400), patch_size=(510, 840)):
         self.input_dir = input_dir
         self.target_dir = target_dir
         self.image_files = [f for f in os.listdir(input_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
         self.image_size = image_size
+        self.patch_size = patch_size
         self.transform = transforms.Compose([
-            transforms.Resize(image_size),
             transforms.ToTensor(),
         ])
 
     def __len__(self):
-        return len(self.image_files)
+        return len(self.image_files) * (
+                    (self.image_size[0] // self.patch_size[0]) * (self.image_size[1] // self.patch_size[1]))
 
     def __getitem__(self, idx):
-        img_name = self.image_files[idx]
+        img_idx = idx // ((self.image_size[0] // self.patch_size[0]) * (self.image_size[1] // self.patch_size[1]))
+        patch_idx = idx % ((self.image_size[0] // self.patch_size[0]) * (self.image_size[1] // self.patch_size[1]))
+
+        img_name = self.image_files[img_idx]
         input_image = Image.open(os.path.join(self.input_dir, img_name)).convert('RGB')
         target_image = Image.open(os.path.join(self.target_dir, img_name)).convert('RGB')
 
-        input_tensor = self.transform(input_image)
-        target_tensor = self.transform(target_image)
+        row = (patch_idx // (self.image_size[1] // self.patch_size[1])) * self.patch_size[0]
+        col = (patch_idx % (self.image_size[1] // self.patch_size[1])) * self.patch_size[1]
+
+        input_patch = input_image.crop((col, row, col + self.patch_size[1], row + self.patch_size[0]))
+        target_patch = target_image.crop((col, row, col + self.patch_size[1], row + self.patch_size[0]))
+
+        input_tensor = self.transform(input_patch)
+        target_tensor = self.transform(target_patch)
 
         return input_tensor, target_tensor
 
@@ -131,25 +142,45 @@ def adjust_hue(image, hue):
     rgb = torch.matmul(hsv_to_rgb, hsv.permute(2, 0, 1))
     return torch.clamp(rgb.permute(1, 2, 0), 0, 1)
 
+@torch.no_grad()
+def apply_model(model, image, patch_size, device):
+    _, h, w = image.shape
+    params = torch.zeros(9, device=device)
+    count = 0
+    for i in range(0, h, patch_size[0]):
+        for j in range(0, w, patch_size[1]):
+            patch = image[:, i:i + patch_size[0], j:j + patch_size[1]].unsqueeze(0)
+            params += model(patch).squeeze(0)
+            count += 1
+    return params / count
 
-def train_model(model, train_loader, criterion, optimizer, num_epochs, device):
+
+def train_model(model, train_loader, criterion, optimizer, num_epochs, device, accumulation_steps=4):
     model.to(device)
+    scaler = GradScaler()
 
     for epoch in range(num_epochs):
         model.train()
         running_loss = 0.0
+        optimizer.zero_grad()
 
-        for inputs, targets in train_loader:
+        for i, (inputs, targets) in enumerate(train_loader):
             inputs, targets = inputs.to(device), targets.to(device)
 
-            optimizer.zero_grad()
-            params = model(inputs)
-            altered_image = apply_transformations(inputs.squeeze(0), params.squeeze(0), device)
-            loss = criterion(altered_image.unsqueeze(0), targets)
-            loss.backward()
-            optimizer.step()
+            with autocast():
+                params = model(inputs)
+                altered_image = apply_transformations(inputs.squeeze(0), params.squeeze(0), device)
+                loss = criterion(altered_image.unsqueeze(0), targets)
 
-            running_loss += loss.item()
+            loss = loss / accumulation_steps
+            scaler.scale(loss).backward()
+
+            if (i + 1) % accumulation_steps == 0:
+                scaler.step(optimizer)
+                scaler.update()
+                optimizer.zero_grad()
+
+            running_loss += loss.item() * accumulation_steps
 
         print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {running_loss / len(train_loader)}")
 
@@ -157,17 +188,20 @@ def train_model(model, train_loader, criterion, optimizer, num_epochs, device):
 def main():
     input_dir = "./source_dir"
     target_dir = "./target_dir"
-    image_size = (5100, 8400)  # Maintaining the original image size
+    image_size = (5100, 8400)
+    patch_size = (510, 840)  # Process 1/10 of the image at a time
 
+    torch.backends.cudnn.benchmark = True
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     model = TransformationParamNet().to(device)
-    dataset = ImageAlterationDataset(input_dir, target_dir, image_size=image_size)
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=True)  # Keeping batch size at 1
+    dataset = PatchedImageDataset(input_dir, target_dir, image_size=image_size, patch_size=patch_size)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2, pin_memory=True)
 
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
-    train_model(model, dataloader, criterion, optimizer, num_epochs=50, device=device)
+    train_model(model, dataloader, criterion, optimizer, num_epochs=50, device=device, accumulation_steps=4)
 
     torch.save(model.state_dict(), "image_alteration_model.pth")
 
